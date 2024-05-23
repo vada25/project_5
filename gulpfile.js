6603f984b519e35d8c966860c057563922876bd2
vada25 <vada1981@gmail.com>
2024-05-23 18:12:27 +0300

    upd 5

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,40 @@
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
 import gulpSass from 'gulp-sass';
-import dartSass from 'sass';
+import * as dartSass from 'sass';
 import cleanCSS from 'gulp-clean-css';
 import autoprefixer from 'gulp-autoprefixer';
 import rename from 'gulp-rename';
 
 // Инициализация gulp-sass с компилятором dartSass
 const sass = gulpSass(dartSass);
+const server = browserSync.create();
 
 // Задача для запуска сервера
 gulp.task('server', function() {
-    browserSync.init({
+    server.init({
         server: {
             baseDir: 'src'
         }
     });
 
-    gulp.watch('src/*.html').on('change', browserSync.reload);
+    gulp.watch('src/*.html').on('change', server.reload);
 });
 
 // Задача для компиляции стилей
 gulp.task('styles', function() {
-    return gulp.src('src/sass/*.+(scss|sass)')
+    return gulp.src('src/sass/**/*.+(scss|sass)')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename({ suffix: '.min', prefix: '' }))
         .pipe(autoprefixer())
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.stream());
+        .pipe(server.stream());
 });
 
 // Задача для отслеживания изменений в файлах стилей
 gulp.task('watch', function() {
-    gulp.watch('src/sass/*.+(scss|sass)', gulp.parallel('styles'));
+    gulp.watch('src/sass/**/*.+(scss|sass)', gulp.series('styles'));
 });
 
 // Задача по умолчанию, запускающая сервер, стили и отслеживание изменений
