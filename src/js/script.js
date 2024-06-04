d82d45e47ec7401d9de527af0086c2dbf671445f
vada25 <vada1981@gmail.com>
2024-06-04 13:37:59 +0300

    upd_21

--- src/js/script.js
+++ src/js/script.js
@@ -34,4 +34,46 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
         
+    //modal
+
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+    });
+   
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+      })
+    });
+
+    
+
+    function validateForms(form) {
+      $(form).validate({
+        rules: {
+          name: "required",
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name: "Пожалуйста введите свое имя",
+          phone: "Пожалуйста введите свой номер телефона",
+          email: {
+            required: "Пожалуйста введите свой имейл",
+            email: "Неправильно введен имейл"
+          }
+        }
+      });
+    }
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
   });
