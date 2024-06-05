e37599c4225d481c7257ae0eb62181b314bb6f71
vada25 <vada1981@gmail.com>
2024-06-05 16:41:02 +0300

    upd_23

--- src/js/script.js
+++ src/js/script.js
@@ -113,9 +113,11 @@ $(document).ready(function(){
 
     //slow scroll 
 
-    $("a[href^='#']").click(function() {
+    $("a[href^=#up]").click(function() {
       const _href = $(this).attr("href");
       $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
       return false;
     });
+
+    new WOW().init();
   });
