f988a327bca96524e8f47e458e032ec1dffa7cc3
vada25 <vada1981@gmail.com>
2024-06-05 15:01:57 +0300

    upd_22

--- src/js/script.js
+++ src/js/script.js
@@ -100,4 +100,22 @@ $(document).ready(function(){
       });
       return false;
     })
+
+    //smooth scroll
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+
+    //slow scroll 
+
+    $("a[href^='#']").click(function() {
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
   });
