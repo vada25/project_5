c09779f33cb21cdf43784ce38e1963cd87c2bef7
vada25 <vada1981@gmail.com>
2024-06-05 14:30:54 +0300

    upd_21

--- src/js/script.js
+++ src/js/script.js
@@ -78,4 +78,26 @@ $(document).ready(function(){
     validateForms('#order form');
 
     $('input[name=phone]').mask("+3 (999) 999-9999");
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+
+      if (!$(this).valid()) {
+        return;
+      }
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function(){
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+
+
+        $('form').trigger('reset');
+      });
+      return false;
+    })
   });
